e7a6e61fbafe691de09931b078aa9d8e1bf46813
Evgenii Troinov <troinof@gmail.com>
2025-08-01 23:23:42 +0200

    master: added years to days fn

---
+++ src/days/third.rs
@@ -0,0 +1,3 @@
+pub fn age_to_day(age: i32) -> i32 {
+    age * 365
+}
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ mod days;
 
 fn main() {
     days::first::hello();
-    print!("a + b = {}", days::second::add(3, 5));
+    println!("a + b = {}", days::second::add(3, 5));
+    println!("5 years is {} days", days::third::age_to_day(5));
 }
